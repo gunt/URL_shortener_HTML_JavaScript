300218cadfa7eece4d64cb21a35236673dcad2b9
gunt <4261629+gunt@users.noreply.github.com>
2019-09-22 15:45:51 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
+
+function getrandom() {
+    var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
+    return random_string()
+}
