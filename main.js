71d5283256286ca9e2930d46628ca4ec31b0988a
gunt <4261629+gunt@users.noreply.github.com>
2019-09-22 15:56:41 -0600

    store the value & check if the URL Protocols are OK

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ function getrandom() {
 }
 
 
-
-// Function to 
-
-function geturl(){     
-    var url = document.getElementById(“urlinput”).value;     
-    var protocol_ok = url.startsWith(“http://”) || url.startsWith(“https://”) || url.startsWith(“ftp://”);     if(!protocol_ok){         newurl = “http://”+url;         return newurl;     }else{         return url;     }
+// // In the geturl function, we first store the value of the input box in the url variable. 
+// Then we check if the URL protocols are OK or not. If the protocol doesn’t start with http:// 
+// , https:// or ftp:// we add http:// at the beginning of the URL then return the URL.
+function geturl() {
+    var url = document.getElementById("urlinput").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if (!protocol_ok) {
+        newurl = "http://" + url;
+        return newurl;
+    } else {
+        return url;
+    }
